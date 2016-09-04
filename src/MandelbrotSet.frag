8f4351dc88137edf28710ea2ca10f1e9319fb7ac
Ark <target.ark@gmail.com>
2016-09-04 09:42:01 +0900

    add comments

--- src/MandelbrotSet.frag
+++ src/MandelbrotSet.frag
@@ -1,6 +1,6 @@
 uniform vec2 resolution;
-uniform float zoom;
-uniform vec2 diff;
+uniform float zoom; // 拡大縮小の倍率
+uniform vec2 diff;  // 平行移動の差分のベクトル
 
 vec2 mult(vec2 c1, vec2 c2) {
     return vec2(c1.x*c2.x - c1.y*c2.y, c1.x*c2.y + c1.y*c2.x);
@@ -23,8 +23,10 @@ float calc(vec2 c) {
 
 void main() {
     vec2 uv = (gl_FragCoord.xy*2.0 - resolution) / min(resolution.x, resolution.y);
-    uv *= 1.5 * zoom;
-    uv -= diff;
+    uv *= 1.5;
+
+    uv *= zoom; // 拡大縮小
+    uv -= diff; // 平行移動
 
     gl_FragColor = vec4(vec3(calc(uv)), 1.0);
 }
--- src/main.kt
+++ src/main.kt
@@ -1,27 +1,26 @@
 import processing.core.PApplet
 import processing.core.PVector
-import processing.event.MouseEvent
+import processing.event.MouseEvent //
 import processing.opengl.PShader
 
 class MainApp: PApplet() {
     private lateinit var sd: PShader
-    private var zoomPower: Float = 0f
-    private var preMousePressed: Boolean = false
-    private var mouseDiff: PVector = PVector(0f, 0f)
+    private var zoomPower: Float = 0f // 拡大縮小の倍率の指数部分
+    private var preMousePressed: Boolean = false // 1フレーム前にマウスが押されているか？
+    private var mouseDiff: PVector = PVector(0f, 0f) // 平行移動のためのベクトル
 
     override fun settings() {
         size(600, 400, P3D)
     }
 
     override fun setup() {
-        background(0f)
         sd = loadShader("MandelbrotSet.frag")
         sd.set("resolution", width.toFloat(), height.toFloat())
     }
 
     override fun draw() {
-        updateMouseDiff()
-        updateUniformVariables()
+        updateMouseDiff() //
+        updateUniformVariables() //
 
         shader(sd)
 
@@ -34,25 +33,28 @@ class MainApp: PApplet() {
         }
         endShape()
 
-        preMousePressed = mousePressed
+        preMousePressed = mousePressed //
     }
 
-    fun updateMouseDiff() {
-        if (mousePressed && preMousePressed) {
-            PVector((mouseX-pmouseX)/width.toFloat()*2f, (mouseY-pmouseY)/height.toFloat()*2f).let {
-                mouseDiff = mouseDiff.add(it.mult(exp(zoomPower)))
-            }
-        }
-    }
+    // マウスドラッグによる差分ベクトルの更新
+    fun updateMouseDiff() { //
+        if (mousePressed && preMousePressed) { //
+            PVector((mouseX-pmouseX)/width.toFloat()*2f, (mouseY-pmouseY)/height.toFloat()*2f).let { //
+                mouseDiff = mouseDiff.add(it.mult(exp(zoomPower))) //
+            } //
+        } //
+    } //
 
-    fun updateUniformVariables() {
-        sd.set("zoom", exp(zoomPower))
-        sd.set("diff", mouseDiff.x, -mouseDiff.y)
-    }
+    // ユニフォーム変数の更新
+    fun updateUniformVariables() { //
+        sd.set("zoom", exp(zoomPower)) //
+        sd.set("diff", mouseDiff.x, -mouseDiff.y) //
+    } //
 
-    override fun mouseWheel(e: MouseEvent) {
-        zoomPower += e.count.toFloat() / 10f
-    }
+    // マウスホイールの変化量を蓄積
+    override fun mouseWheel(e: MouseEvent) { //
+        zoomPower += e.count.toFloat() / 10f //
+    } //
 }
 
 fun main(args: Array<String>) = PApplet.main(MainApp().javaClass.name)
